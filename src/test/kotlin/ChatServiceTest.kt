0eab7948359806cb13580e6cf73ace362335cf04
ERokhmanko <rizhii_mishonok@mail.ru>
2021-07-19 15:43:13 +0300

    test object

--- src/test/kotlin/ChatServiceTest.kt
+++ src/test/kotlin/ChatServiceTest.kt
@@ -1,13 +1,15 @@
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.jupiter.api.assertAll
+
+//import org.hamcrest.Matchers.containsInAnyOrder
 
 class ChatServiceTest {
 
     @Test
     fun addUser() {
         val service = ChatService()
-       val result = service.addUser("Mish")
+        val result = service.addUser("Mish")
         assertEquals(1, result)
     }
 
@@ -20,8 +22,10 @@ class ChatServiceTest {
 
         val result = service.getUsersList()
 
-        val truResult = mutableListOf<User>(User(1, "Mish"), User(2, "Mish2"),
-            User(3, "Mish3"))
+        val truResult = mutableListOf<User>(
+            User(1, "Mish"), User(2, "Mish2"),
+            User(3, "Mish3")
+        )
 
         assertEquals(truResult, result)
     }
@@ -43,14 +47,14 @@ class ChatServiceTest {
         val mess2 = Message(idMessage = 0, userFromId = 1, userToId = 3, idChat = 1, text = "tljfdl")
         val result = service.addMess(mess2)
 
-            assertTrue(result)
+        assertTrue(result)
     }
 
     @Test
     fun delMessTrue() {
         val service = ChatService()
         val mess1 = Message(idMessage = 0, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl")
-       service.addMess(mess1)
+        service.addMess(mess1)
 
         val result = service.delMess(1, 1)
         assertTrue(result)
@@ -72,7 +76,7 @@ class ChatServiceTest {
         val mess1 = Message(idMessage = 0, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl")
         service.addMess(mess1)
 
-        val result = service.editMess(1,1, "edit")
+        val result = service.editMess(1, 1, "edit")
 
         assertTrue(result)
     }
@@ -83,7 +87,7 @@ class ChatServiceTest {
         val mess1 = Message(idMessage = 0, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl")
         service.addMess(mess1)
 
-        val result = service.editMess(2,2, "edit")
+        val result = service.editMess(2, 2, "edit")
 
         assertFalse(result)
     }
@@ -94,9 +98,20 @@ class ChatServiceTest {
         service.addMess(Message(idMessage = 0, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl"))
 
         val result = service.getMess(1, 1)
-        val trueResult = "[Message(idMessage=1, userFromId=1, userToId=2, idChat=1, time=18/07/2021 04:04:15, text=tljfdl, viewed=true)]"
 
-        assertEquals(trueResult, result)
+        val trueResult =
+            Message(idMessage = 1, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl", viewed = true)
+
+        assertAll(
+            "result",
+            { assertEquals(trueResult.idMessage, result.get(0).idMessage) },
+            { assertEquals(trueResult.userFromId, result.get(0).userFromId) },
+            { assertEquals(trueResult.userToId, result.get(0).userToId) },
+            { assertEquals(trueResult.idChat, result.get(0).idChat) },
+            { assertEquals(trueResult.text, result.get(0).text) },
+            { assertEquals(trueResult.viewed, result.get(0).viewed) }
+        )
+
     }
 
     @Test
@@ -120,7 +135,42 @@ class ChatServiceTest {
     }
 
     @Test
-    fun getChatsByUserId() { //TODO
+    fun getChatsByUserIdTrue() {
+        val service = ChatService()
+        service.addMess(Message(idMessage = 0, userFromId = 1, userToId = 2, idChat = 1, text = "tljfdl"))
+
+        val result = service.getChatsByUserId(1)
+
+        val trueResult = listOf<Chat>(
+            Chat(
+                1, 1, 2,
+                mutableListOf<Message>(
+                    Message(
+                        idMessage = 1,
+                        userFromId = 1,
+                        userToId = 2,
+                        idChat = 1,
+                        text = "tljfdl",
+                        viewed = false
+                    )
+                )
+            )
+        )
+
+
+        assertAll(
+            "result",
+            { assertEquals(trueResult.get(0).idChat, result.get(0).idChat) },
+            { assertEquals(trueResult.get(0).userFromId, result.get(0).userFromId) },
+            { assertEquals(trueResult.get(0).userToId, result.get(0).userToId) },
+            { assertEquals(trueResult.get(0).messages.get(0).idMessage, result.get(0).messages.get(0).idMessage) },
+            { assertEquals(trueResult.get(0).messages.get(0).userFromId, result.get(0).messages.get(0).userFromId) },
+            { assertEquals(trueResult.get(0).messages.get(0).userToId, result.get(0).messages.get(0).userToId) },
+            { assertEquals(trueResult.get(0).messages.get(0).idChat, result.get(0).messages.get(0).idChat) },
+            { assertEquals(trueResult.get(0).messages.get(0).text, result.get(0).messages.get(0).text) },
+            { assertEquals(trueResult.get(0).messages.get(0).viewed, result.get(0).messages.get(0).viewed) }
+        )
+
     }
 
     @Test
@@ -134,6 +184,6 @@ class ChatServiceTest {
     }
 
     @Test
-    fun searchChatId() { //TODO
+    fun searchChatId() {
     }
 }
